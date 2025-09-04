677100c42e5f27609520a1a3b0b7ee42637d6542
rsq-codequeen <159681694+rsq-codequeen@users.noreply.github.com>
2025-09-04 13:20:21 +0500

    Revert "added lazy loading"

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import "./App.css";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products"
 import NavigationBar from "./components/NavigationBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import Pricing from "./Pages/Pricing";
+import Services from "./Pages/Services";
 import FlowbiteInitializer from "./components/FLowbiteInitializer";
-import React, { Suspense } from 'react';
 
-// Lazy-loaded components
-const Home = React.lazy(() => import('./Pages/Home'));
-const Products = React.lazy(() => import('./Pages/Products'));
-const Pricing = React.lazy(() => import('./Pages/Pricing'));
-const Services = React.lazy(() => import('./Pages/Services'));
-const Blog = React.lazy(() => import('./Pages/Blog'));
-const AboutUs = React.lazy(() => import('./Pages/AboutUs'));
-
+import React  from 'react';
+import AboutUs from "./Pages/AboutUs";
+import Blog from "./Pages/Blog";
 function App() {
+
   return (
     <Router basename="/frontend-sps">
-      <FlowbiteInitializer />
+      <FlowbiteInitializer /> 
       <NavigationBar />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/pricing/:planName" element={<Pricing />} />
-          <Route path="/services/:serviceName" element={<Services />} />
-          <Route path="/activities/blog" element={<Blog />} />
-          <Route path="/about-us/contact" element={<AboutUs />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/pricing/:planName" element={<Pricing />} />
+        <Route path="/services/:serviceName" element={<Services/>}/>
+        <Route path="/activities/blog" element={<Blog/>}/>
+        <Route path="/about-us/contact" element={<AboutUs/>}/>
+      </Routes>
       <Footer />
     </Router>
   );
